7734f5c85df0f3e7d3623c88eb76ed10228ae86d
mkg20001 <mkg20001@gmail.com>
2018-01-28 13:07:43 +0100

    Fix secio transport

--- src/transport/secio.js
+++ src/transport/secio.js
@@ -4,10 +4,9 @@ const secio = require('libp2p-secio')
 
 module.exports = function SecioCrypto (protocol, zeronet, id) {
   protocol.crypto.add('secio', (conn, conf, cb) => {
-    try {
-      cb(null, secio.encrypt(id, id.privKey, conn))
-    } catch (e) {
-      cb(e)
-    }
+    const enc = secio.encrypt(id, conn, err => {
+      if (err) return cb(err)
+      cb(null, enc)
+    })
   })
 }
